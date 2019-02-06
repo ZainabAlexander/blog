403eeb5d742d56283bb8aac6b19868cc525d2587
Zainab <zainabalexander@gmail.com>
2019-02-06 12:50:32 +0200

    added pages

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var createRouter = require('./routes/create');
+var archiveRouter = require('./routes/archive')
+
+// for create function
 
 var app = express();
+const bodyParser= require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/create',createRouter);
+app.use('/archive', archiveRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
+
 var express = require('express');
 var router = express.Router();
+var Pokemon = require('../db.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Let's Get Baked!"});
+  res.render('index', {title: "lets  get baked"});
+
+});
+
+router.get('/', function (req, res, next) {
+
+    let data = {
+        title: 'All Pokemon',
+        pokemon: Pokemon, 
+        message: false,
+    }
+
+    res.render('index', data);
+
 });
 
 module.exports = router;
